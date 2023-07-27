18139a8bbacb24534b99140586aa4dec13b3714f
yic <zodiac036@gmail.com>
2023-07-27 01:10:12 +0000

    update train / run script to take external outputs

--- run.sh
+++ run.sh
@@ -1,26 +1,73 @@
 #!/bin/bash
+# Example usage. Put wav files under
+# input
+#  -dataset1
+#   -test.wave
+# and run ./run.sh model_name dataset1
 
-DATA_SET_NAME="kenny"
+INPUT_FOLDER="input"
+TRAINED_MODEL_FOLDER="trained"
 
-# python test.py --pose data/kenny/transforms_val.json \
-#     --ckpt trial_kenny_torso/checkpoints/ngp.pth \
-#     --aud data/test/ian_16sec_eo.npy \
-#     --workspace trial_kenny_test \
+if [ -z "$1" ]; then
+  echo "Error: Dataset name not provided."
+  echo "Usage: $0 <model_name> <input_dataset_name>"
+  exit 1
+fi
+
+MODEL_NAME=$1
+DATASET_FOLDER=$INPUT_FOLDER/$2
+
+# Check if the folder exists
+if [ ! -d "$DATASET_FOLDER" ]; then
+  echo "Error: Folder '$DATASET_FOLDER' not found."
+  exit 1
+fi
+
+# Find all WAV files in the folder
+WAV_FILES=$(find "$DATASET_FOLDER" -type f -iname "*.wav")
+echo $WAV_FILES
+
+# Process each WAV file
+for file_path in $WAV_FILES; do
+  # Get the file name without the extension
+  file_name=$(basename "$file_path" .wav)
+  echo $file_name
+  
+  # Create a folder with the same name as the file (if it doesn't exist)
+  folder_name="$DATASET_FOLDER/$file_name"
+  if [ ! -d "$folder_name" ]; then
+    mkdir "$folder_name"
+  fi
+  echo $folder_name
+
+  # Move the file into its corresponding folder
+  mv "$file_path" "$folder_name/"
+
+  # Create npy file
+  conda run -n pytorch3d python nerf/asr.py --wav $folder_name/${file_name}.wav --save_feats
+
+#   # Run the synthesis
+#   conda run -n pytorch3d python main.py \
+#     --workspace $TRAINED_MODEL_FOLDER/$MODEL_NAME/${MODEL_NAME}_eo_torso \
+#     --aud $folder_name/${file_name}.npy \
+#     --torso \
+#     --smooth_path_window 50 \
+#     --data_range 0 -2 \
 #     -O \
-#     --torso
+#     --test 
 
-# python main.py --pose data/kenny/transforms_val.json \
-#     --ckpt trial_kenny_torso/checkpoints/ngp.pth \
-#     --aud data/test/ian_16sec_eo.npy \
-#     --workspace trial_kenny_test \
-#     -O \
-#     --torso
+    # Run the synthesis
+    conda run -n pytorch3 python test.py --pose $TRAINED_MODEL_FOLDER/$MODEL_NAME/transforms_val.json \
+        --ckpt $TRAINED_MODEL_FOLDER/$MODEL_NAME/${MODEL_NAME}_eo_torso/checkpoints/ngp.pth \
+        --aud $folder_name/${file_name}.npy \
+        --workspace $folder_name \
+        --smooth_path_window 50 \
+        --data_range 0 -2 \
+        -O \
+        --torso
+done
 
-python main.py data/$DATA_SET_NAME \
-    --workspace trial_${DATA_SET_NAME}_eo_torso \
-    --aud data/test/ian_16sec_eo.npy \
-    --torso \
-    --smooth_path_window 50 \
-    --data_range 0 -2 \
-    -O \
-    --test 
+
+
+
+
--- train.sh
+++ train.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 DATA_FOLDER="data"
-OUTPUT_FOLDER="trained"
+TRAINED_MODEL_FOLDER="trained"
+DEFAULT_ITERATION=200000
 
 if [ -z "$1" ]; then
   echo "Error: Dataset name not provided."
@@ -17,10 +18,10 @@ mkdir -p $DATASET_FOLDER
 conda run -n pytorch3d python data_utils/process.py $DATASET_FOLDER/$DATASET_NAME.mp4
 
 # train
-CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $OUTPUT_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --iters 200000
-CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $OUTPUT_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --finetune_lips --iters 250000
+CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --iters 10000
+CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --finetune_lips --iters 15000
 
-CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $OUTPUT_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --iters 200000 --head_ckpt $OUTPUT_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/checkpoints/ngp.pth
+CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --iters 10000 --head_ckpt $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/checkpoints/ngp.pth
 
 # test
-CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $OUTPUT_FOLDER/$DATA_SET_NAME/ --workspace $OUTPUT_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --test
+CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --test
