0522060bd2b899b08d6502c25af0bcd4488fa3ee
yic <zodiac036@gmail.com>
2023-07-27 00:26:17 +0000

    made train script take input argument

--- train.sh
+++ train.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-DATA_SET_NAME="kenny"
+DATA_FOLDER="data"
+OUTPUT_FOLDER="trained"
+
+if [ -z "$1" ]; then
+  echo "Error: Dataset name not provided."
+  echo "Usage: $0 <video_file_name>"
+  exit 1
+fi
+
+DATASET_NAME=$(basename "$1")
+DATASET_FOLDER=$DATA_FOLDER/$DATASET_NAME
 
 # data processing
-conda run -n pytorch3d python data_utils/process.py data/$DATA_SET_NAME/$DATA_SET_NAME.mp4
+mkdir -p $DATASET_FOLDER
+conda run -n pytorch3d python data_utils/process.py $DATASET_FOLDER/$DATASET_NAME.mp4
 
 # train
-CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py data/${DATA_SET_NAME}/ --workspace trial_${DATA_SET_NAME}_eo/ -O --iters 200000
-CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py data/${DATA_SET_NAME}/ --workspace trial_${DATA_SET_NAME}_eo/ -O --finetune_lips --iters 250000
+CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $OUTPUT_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --iters 200000
+CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $OUTPUT_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --finetune_lips --iters 250000
 
-CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py data/${DATA_SET_NAME}/ --workspace trial_${DATA_SET_NAME}_eo_torso/ -O --torso --iters 200000 --head_ckpt trial_${DATA_SET_NAME}_eo/checkpoints/ngp.pth
+CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $OUTPUT_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --iters 200000 --head_ckpt $OUTPUT_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/checkpoints/ngp.pth
 
 # test
-CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py data/${DATA_SET_NAME}/ --workspace trial_${DATA_SET_NAME}_eo_torso/ -O --torso --test
+CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $OUTPUT_FOLDER/$DATA_SET_NAME/ --workspace $OUTPUT_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --test
