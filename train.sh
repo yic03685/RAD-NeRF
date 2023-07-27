9b73cfa88e4ec509313a7a0fdd05e6e2d63f321c
yic <zodiac036@gmail.com>
2023-07-27 01:37:28 +0000

    added step debug info in training script

--- train.sh
+++ train.sh
@@ -17,15 +17,19 @@ DATASET_FOLDER=$DATA_TEMP_FOLDER/$DATASET_NAME
 # data processing
 mkdir -p $DATASET_FOLDER
 cp $DATA_INPUT_FOLDER/$DATASET_NAME/${DATASET_NAME}.mp4 $DATASET_FOLDER/${DATASET_NAME}.mp4
+echo ==Start Pre-process==
 conda run -n pytorch3d python data_utils/process.py $DATASET_FOLDER/$DATASET_NAME.mp4
 
 # train
+echo ==Start Overll Training==
 CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --iters 10000
+echo ==Start Lips Finetuning==
 CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --finetune_lips --iters 15000
-
+echo ==Start Torso Training==
 CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --iters 10000 --head_ckpt $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/checkpoints/ngp.pth
 
 # test
+echo ==Running Tests==
 CUDA_VISIBLE_DEVICES=0 conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --test
 
 # remove temp files
