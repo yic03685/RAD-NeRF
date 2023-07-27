2d68254719afa104a4056d6c8fc24341cd799bde
yic <zodiac036@gmail.com>
2023-07-27 05:33:52 +0000

    separate conda activation

--- train.sh
+++ train.sh
@@ -14,23 +14,26 @@ fi
 DATASET_NAME=$(basename "$1")
 DATASET_FOLDER=$DATA_TEMP_FOLDER/$DATASET_NAME
 
+# use the conda pytorch environment
+conda activate pytorch3d
+
 # data processing
 mkdir -p $DATASET_FOLDER
 cp $DATA_INPUT_FOLDER/$DATASET_NAME/${DATASET_NAME}.mp4 $DATASET_FOLDER/${DATASET_NAME}.mp4
 echo ==Start Pre-process==
-eval "$(conda run -n pytorch3d python data_utils/process.py $DATASET_FOLDER/$DATASET_NAME.mp4)"
+python data_utils/process.py $DATASET_FOLDER/$DATASET_NAME.mp4
 
 # train
 echo ==Start Overll Training==
-CUDA_VISIBLE_DEVICES=0 eval "$(conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --iters 10000)"
+CUDA_VISIBLE_DEVICES=0 python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --iters 10000
 echo ==Start Lips Finetuning==
-CUDA_VISIBLE_DEVICES=0 eval "$(conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --finetune_lips --iters 15000)"
+CUDA_VISIBLE_DEVICES=0 python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/ -O --finetune_lips --iters 15000
 echo ==Start Torso Training==
-CUDA_VISIBLE_DEVICES=0 eval "$(conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --iters 10000 --head_ckpt $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/checkpoints/ngp.pth)"
+CUDA_VISIBLE_DEVICES=0 python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --iters 10000 --head_ckpt $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo/checkpoints/ngp.pth
 
 # test
 echo ==Running Tests==
-CUDA_VISIBLE_DEVICES=0 eval "$(conda run -n pytorch3d python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --test)"
+CUDA_VISIBLE_DEVICES=0 python main.py $DATASET_FOLDER/ --workspace $TRAINED_MODEL_FOLDER/$DATASET_NAME/${DATA_SET_NAME}_eo_torso/ -O --torso --test
 
 # remove temp files
 rm -rf $DATA_TEMP_FOLDER
